19cc1aeccf65577bb0ede20a0e4624dbf757a3bd
Никита <nikita@MacBook-Air-Nikita.local>
2026-01-11 23:21:05 +0300

    добавил поиск в экран со списком, всё ок

--- src/screens/SearcEvents/SearchEventsScreen.tsx
+++ src/screens/SearcEvents/SearchEventsScreen.tsx
@@ -1,16 +1,33 @@
 import EventList from '@/components/EventList';
 import { useEvents } from '@/context';
 import { useRouter } from 'expo-router';
-import { View } from 'react-native';
+import { useState } from 'react';
+import { TextInput, View } from 'react-native';
 
 export default function SearchEventsScreen() {
   const { events } = useEvents();
   const router = useRouter();
+  const [query, setQuery] = useState('');
+
+  const filteredEvents = events.filter(event =>
+    event.title.toLowerCase().includes(query.toLowerCase())
+  );
 
   return (
     <View style={{ flex: 1 }}>
+      <TextInput
+        placeholder="Поиск мероприятия"
+        value={query}
+        onChangeText={setQuery}
+        style={{
+          padding: 12,
+          borderBottomWidth: 1,
+          borderColor: '#ddd',
+        }}
+      />
+
       <EventList
-        events={events}
+        events={filteredEvents}
         onSelect={(event) => router.push(`/events/${event.id}`)}
       />
     </View>
